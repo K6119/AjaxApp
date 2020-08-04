2631b327a5dc6d4e7601012212dcadc463505771
K6119 <k.adsqu2@gmail.com>
2020-08-04 17:58:01 +0900

    既読機能のサバーサイドの記述

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,12 +1,22 @@
-class PostsController < ApplicstionController
+class PostsController < ApplicationController
   def index
-    @posts = post.all
-  end
-
-  def new
+    @posts = Post.all.order(id: "DESC")
   end
 
   def create
     Post.create(memo: params[:memo])
+    redirect_to action: :index
+  end
+
+  def checked
+    post = Post.find(params[:id])
+    if post.checked then
+      post.update(checked: false)
+    else
+      post.update(checked: true)
+    end
+
+    item = Post.find(params[:id])
+    render json: { post: item }
   end
 end
